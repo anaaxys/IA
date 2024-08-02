8825bdbb969e181c4c1e22ea79ec17040207fad0
Ana Alura <105944221+anaaxys@users.noreply.github.com>
2024-08-02 11:08:28 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,8 @@
+const nomes = ["Ana", "Estefany", "Adrieli", "Aime", "Emilli"];
+
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
     return lista[posicao];
 }
+
+export const nome = aleatorio(nomes);
