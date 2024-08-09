1a8c9a143ab0f6c0491f54a1eddb509fc46fd299
Ana Alura <105944221+anaaxys@users.noreply.github.com>
2024-08-09 08:19:50 -0300

    Add files via upload

--- js/perguntas.js
+++ js/perguntas.js
@@ -27,7 +27,7 @@ export const perguntas = [
                     "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
                     "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
                 ],
-                próximo: 3,
+                proxima: 3,
             },
             {
                 texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
@@ -35,7 +35,7 @@ export const perguntas = [
                     "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
                     "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
                 ],
-                próximo: 4,
+                proxima: 4,
             },
         ]
     },
@@ -47,14 +47,14 @@ export const perguntas = [
                 afirmacao: ["Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
                     "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
                 ],
-                próximo: 5,
+                proxima: 5,
             },
             {
                 texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
                 afirmacao: ["Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
                     "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
                 ],
-                próximo: 6,
+                proxima: 6,
             },
         ]
     },
@@ -66,7 +66,7 @@ export const perguntas = [
                 afirmacao: ["Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
                     "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
                 ],
-                próximo: 7,
+                proxima: 7,
             },
             {
                 texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
@@ -74,7 +74,7 @@ export const perguntas = [
                     "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
                     "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
                 ],
-                próximo: 8,
+                proxima: 8,
             },
         ]
     },
